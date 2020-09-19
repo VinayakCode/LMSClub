6cdecfd8d4f3ec7ba9ee0e3f2e2172279f3d971e
bar <foo>
2020-09-19 09:22:12 -0700

    School Schedule.py added

---
+++ Callender/schoolshedule.py
@@ -0,0 +1,6 @@
+a = """Day          Time.           Subject
+Monday   10:00 - 11:00 AM    Period 1
+                  11:30-12:30 AM.    Math
+Tuesday     9:00-11:00 AM     PE
+                  10:00-11:00 AM.    Science"""
+print(a)
